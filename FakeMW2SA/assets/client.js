c1165783b32efa9abe1d8d605d281bbcc8726ee9
Floris Roozemond <florisroozemond@outlook.com>
2020-07-10 06:01:44 +0200

    formatting and styling

--- FakeMW2SA/assets/client.js
+++ FakeMW2SA/assets/client.js
@@ -1,38 +1,50 @@
 
-function reload() {
-    jsonresponse = $.ajax("/?action=players").done(function () {
+function reload()
+{
+    // ? https://api.jquery.com/jQuery.ajax/ > var jqxhr = $.ajax("example.php").done(function() {
+    jsonresponse = $.ajax("/?action=players").done(function ()
+    {
+        // https://api.jquery.com/jQuery.ajax/ > responseJSON
         data = jsonresponse.responseJSON["players"];
         host = jsonresponse.responseJSON["host"];
         ipaddresses = jsonresponse.responseJSON["ipaddresses"];
 
         var partygroups = [];
 
-        for (i = 0; i < data.length; i++) {
-            var temp = data.filter(function (a) {
+        for (i = 0; i < data.length; i++)
+        {
+            var temp = data.filter(function (a)
+            {
                 return (a.partyID == i);
             });
 
-            if (temp[0] != null) {
+            if (temp[0] != null)
+            {
                 lastseens = [];
                 temp2 = [];
 
-                temp.forEach(function (item) {
+                temp.forEach(function (item)
+                {
                     lastseens.push(item.lastseen);
                 });
 
                 biggest = Math.max(...lastseens);
 
-                temp.forEach(function (item) {
-                    if (item.lastseen != biggest) {
+                temp.forEach(function (item)
+                {
+                    if (item.lastseen != biggest)
+                    {
                         item.partyID = 0;
                         partygroups.push([item]);
                     }
-                    else {
+                    else
+                    {
                         temp2.push(item);
                     }
                 });
 
-                if (temp2.length > 0) {
+                if (temp2.length > 0)
+                {
                     partygroups.push(temp2);
                 }
             }
@@ -82,7 +94,7 @@ function playerlocation(player) {
 
 function checkban(player) {
     if (player.banned == "True") {
-        return " bg-danger"
+        return "bg-danger"
     } else {
         return ""
     }
@@ -102,7 +114,7 @@ function populate()
 
     var content = `
         <table class="table table-striped table-hover table-bordered" class="players">
-            <thead>
+            <thead style="background-color:silver">
                 <tr>
                     <th>Name</th>
                     <th>VAC</th>
@@ -116,30 +128,22 @@ function populate()
     {
         if (player.memberjoin == false)
         {
-            content += '<tr class="' + player.partyID + '">';
+            content += "<tr class='" + player.partyID + "'>";
 
             // player name
             content += "<td class='name " + checkban(player) + "'><a class='dropdown-toggle " + playerhost(player) + "' href='#' role='button' id='dropdownMenuLink' data-toggle='dropdown' aria-haspopup='true' aria-expanded='false'><span>" + escape(player.personaname) + "</span></a>";
-            //content += '<td class="name ' + checkban(player) + '"><a class="dropdown-toggle ' + playerhost(player) + '" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><span>' + escape(player.personaname) + '</span></a>';
-
-            content += '<div class="dropdown-menu" aria-labelledby="dropdownMenuLink">';
-
-            var text = `${player.steamid}\\n${strip(player.personaname)}\\n${player.ip}`;
-
+            // dropdown
+            content += "<div class='dropdown-menu' aria-labelledby='dropdownMenuLink'>";
             // view profile
-            //content += "<a class='dropdown-item' href='" + player.profileurl + "' target='_blank'>Profile</a>";
-            content += '<a class="dropdown-item" href="' + player.profileurl + '" target="_blank">Profile</a>';
-
-            // copy user info
-            content += "<a class='dropdown-item' href='#' onclick=\"copyTextToClipboard('" + text + "')\">Copy</a>";
-
+            content += "<a class='dropdown-item' href='" + player.profileurl + "' target='_blank'>Profile</a>";
+            // copy user info content
+            var text = `${player.steamid}\\n${strip(player.personaname)}\\n${player.ip}`;
+            // copy user info button
+            content += "<a class='dropdown-item' href='#' onclick=copyTextToClipboard('" + text + "')>Copy</a>";
             // ban / unban
-            if ((player.banned == null) || (player.banned == "False"))
-            {
+            if ((player.banned == null) || (player.banned == "False")) {
                 content += "<a class='dropdown-item' href='#' onclick=\"ban('" + player.ip + "', '" + strip(player.personaname) + "')\">Ban</a></div></td>";
-            }
-            else
-            {
+            } else {
                 content += "<a class='dropdown-item' href='#' onclick=\"unban('" + player.ip + "', '" + strip(player.personaname) + "')\">Unban</a></div></td>";
             }
 
@@ -148,7 +152,7 @@ function populate()
             {
                 if (player.vacbypass == 0)
                 {
-                    content += '<td class="vac"><div class="btn btn-success btn-sm">' + vac(player) + '</div></td>';
+                    content += '<td class="vac"><button style="background-color: green">' + vac(player) + '</button></td>';
                 }
                 if (player.vacbypass == 1)
                 {
@@ -161,8 +165,9 @@ function populate()
             }
             else
             {
-                content += '<td class="vac">' + vac(player) + '</td>';
+                content += '<td class="vac"><button style="background-color:lightblue">' + vac(player) + '</button></td>';
             }
+
             //Player location
             if (player.ip == "0.0.0.0" || player.ip == "1.3.3.7" || player.ip == "127.0.0.1" || player.ip == "255.255.255.255") {
                 content += '<td class="location"><div class="btn btn-danger btn-sm">' + playerlocation(player) + '</div></td>';
@@ -180,7 +185,7 @@ function populate()
         }
     }
 
-    content += "</table>";
+    content += '</table>';
 
     function nothing() {
         return;
