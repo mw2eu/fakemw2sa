f7de6e82e9af2a004378326c77aba074104ecdfe
florisroozemond <florisroozemond@outlook.com>
2020-12-05 21:39:52 +0100

    unused

--- FakeMW2SA/assets/client.js
+++ FakeMW2SA/assets/client.js
@@ -221,35 +221,6 @@ function escape(args) {
     return s = s.replace(/<span class=\"color[0-9]\"><\/span>/g, "");
 }
 
-function strip(args) {
-    if ("string" !== typeof args) {
-        return "";
-    }
-    if ("" == args.replace(/\^[0-9]/g, "").replace(/ /g, "")) {
-        return "Unnamed player";
-    }
-    args = args.replace(/&/g, "");
-    args = args.replace(/>/g, "");
-    args = args.replace(/</g, "");
-    args = args.replace(/'/g, "\\&apos;");
-    args = args.replace(/"/g, "\\&quot;");
-    var i = 0;
-    s = ""
-    for (; i < args.length; i++) {
-        if ("^" == args[i] && args[i + 1]) {
-            var checked = args.charCodeAt(i + 1);
-            if (48 <= checked && 57 >= checked) {
-                i++;
-            } else {
-                s += args[i];
-            }
-        } else {
-            s += args[i];
-        }
-    }
-    return s;
-}
-
 function copyTextToClipboard(text) {
     var textArea = document.createElement("textarea");
     textArea.value = text;
